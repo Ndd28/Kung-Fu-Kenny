390554ba120e3202c11a113a1616f57c0140f4fe
Fox McCloud <nate_1997@hotmail.com>
2025-07-14 18:05:30 -0400

    V2
    Extended

--- Dasani.sh
+++ Dasani.sh
@@ -2,13 +2,18 @@
 #Lets Learn
 
 a=1
-b=2
+b=0
 c=10
 Sum=$((a+b))
 
 while [ $Sum -le $c ]; do
-	((Sum ++))
-	echo "Round $Sum Approaches"
+	if [ $Sum -lt $c ]; then
+		echo "Round $Sum Approaches" 
+		echo "$((c-Sum)) Rounds Remaining" 
+	else
+		echo "Round $Sum , FINAL ROUND!"
+	fi
+		((Sum++))
 done
 
 
